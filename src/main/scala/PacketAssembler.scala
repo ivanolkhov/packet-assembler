fe7198207e4d0494d6df6d7d77a7d4f2d74b131c
Ivan <ivanolkhov@gmail.com>
2020-09-29 18:24:32 +0100

    add parser

--- src/main/scala/PacketAssembler.scala
+++ src/main/scala/PacketAssembler.scala
@@ -1,11 +1,19 @@
 object PacketAssembler {
-    def splitLine(text: String):  (Int, Int, Int, String) = {
-      val textarray = text.split("\\s+")
-      val part1 = textarray(0).toInt
-      val part2 = textarray(1).toInt
-      val part3 = textarray(2).toInt
-      val part4 = textarray.slice(3, textarray.length).mkString(" ")
-      (part1, part2, part3, part4)
-    }
+  def splitLine(text: String): (Int, Int, Int, String) = {
+    val textarray = text.split("\\s+")
+    val part1 = textarray(0).toInt
+    val part2 = textarray(1).toInt
+    val part3 = textarray(2).toInt
+    val part4 = textarray.slice(3, textarray.length).mkString(" ")
+    (part1, part2, part3, part4)
+  }
+
+  def parser(tuple: (Int, Int, Int, String),
+             acc: Map[Int, List[(Int, Int, String)]]): Map[Int, List[(Int, Int, String)]] = {
+    val msgid = tuple._1
+    val content = (tuple._2, tuple._3, tuple._4)
+    acc + (msgid -> acc.get(msgid).map(l => content::l).getOrElse(List(content)))
+
+  }
 
 }
--- src/test/scala/PacketAssemblerTest.scala
+++ src/test/scala/PacketAssemblerTest.scala
@@ -5,5 +5,11 @@ class PacketAssemblerTest extends AnyFunSuite {
     assert(PacketAssembler.splitLine("6220 1 10	Because he's the hero Gotham deserves,") === (6220, 1, 10, "Because he's the hero Gotham deserves,"))
 
   }
+  test("Parser test") {
+    val m1 = PacketAssembler.parser((10, 1, 10, "hello"), Map())
+    assert(m1 === Map(10 -> List((1, 10, "hello"))))
+    val m2 = PacketAssembler.parser((10, 5, 10, "world"), m1)
+    assert(m2 === Map(10 -> List((5, 10, "world"),(1, 10, "hello"))))
+  }
 
 }
